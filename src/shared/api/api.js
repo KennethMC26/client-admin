eb10af9ea767c88e5e5fe6741244dae72fb8da65
KennethMC26 <Kmazariegos-2021255@kinal.edu.gt>
2026-04-27 11:48:18 -0600

    Conexion con server admin

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -11,9 +11,27 @@ const axiosAuth = axios.create({
     },
 });
 
+const axiosAdmin = axios.create({
+  baseURL: import.meta.env.VITE_ADMIN_URL,
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
 //Cnfiguracion de interceptores
 axiosAuth.interceptors.request.use((config) => {
     config._axiosClient = 'auth';
+    const token = useAuthStore.getState().token;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+axiosAdmin.interceptors.request.use((config) => {
+    config._axiosClient = 'admin';
     const token = useAuthStore.getState().token;
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -105,9 +123,8 @@ const handleRefreshToken = async function (_error) {
 
 axiosAuth.interceptors.response.use((res) => res, handleRefreshToken);
 
-//axiosAdmin.interceptors.response.use((res) => res, handleRefreshToken);
+axiosAdmin.interceptors.response.use((res) => res, handleRefreshToken);
 
 // ================= EXPORT AXIOS =================
-//export { axiosAuth, axiosAdmin };
-export { axiosAuth };
+export { axiosAuth, axiosAdmin };
 export { handleRefreshToken };
